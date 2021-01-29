472dcdf2ba7a16f90f3d0529f965ba562e2de1e7
harshit100306 <harshit100306@gmail.com>
2021-01-29 17:22:37 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,platform;
 
 function setup(){
     var canvas = createCanvas(1200,400);
@@ -11,7 +11,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -41,6 +42,7 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
+    platform.display();
     pig1.display();
     log1.display();
 
